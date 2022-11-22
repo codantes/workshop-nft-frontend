9fa6b81f00bfeb81a0ba8c12ede146dfff8d8f50
unknown <pranavismic@gmail.com>
2022-11-22 14:28:41 +0530

    added uri and mint functions

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,9 @@ const Home = () => {
   const [isOwner, setOwner] = useState(false)
   const [publicAddress, setPublicAddres] = useState("")
   const [loading, setLoading] = useState(false)
+  const [uriUrl, setUriUrl] = useState("")
+  const [copies, setCopies] = useState(0)
+  const [tokenId, setTokenId] = useState(0)
 
   const web3ModalRef = useRef();
 
@@ -56,10 +59,29 @@ const Home = () => {
       const _publicAddress = await signer.getAddress()
       if(_publicAddress === _owner){
         setOwner(true)
-        console.log(publicAddress) 
-        console.log(_owner)
-        console.log(isOwner)
       }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const fixUri = async() => {
+    try {
+      const signer = await getProviderOrSigner(true)
+      const nftContract = new Contract(contractAddress, abi, signer)
+      const res = await nftContract.setURI(uriUrl)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const mint = async (e) => {
+    try {
+      const signer = await getProviderOrSigner(true)
+      const nftContract = new Contract(contractAddress, abi, signer)
+      const response = await nftContract.mint(publicAddress, tokenId, copies, [])
+      window.alert(response)
     } catch (error) {
       console.log(error)
     }
@@ -70,12 +92,53 @@ const Home = () => {
       {
         (isWalletConnected) ? (
           <div>
-            <h1>Connected address {publicAddress}</h1>
+            <h1 className='bg-blue-900 text-white rounded-md p-3 m-4'>Connected address {publicAddress}</h1>
           </div>
         ) : (
           <div>
             <h1>Install Metamask</h1>
           </div>
+        )
+      }
+      {
+        (isWalletConnected)? 
+        (
+          <div>
+            <section className='flex justify-evenly p-2 border-2 bg-blue-400'>
+              <input 
+              type={'text'} 
+              value={uriUrl} 
+              className='border-2 '
+              placeholder='URI url'
+              onChange={(e)=> setUriUrl(e.target.value)}/>
+              <button onClick={fixUri} className='bg-blue-900 text-white rounded-md p-2 m-1'>
+                Set URI
+              </button>
+            </section>
+            <section className='flex p-2 items-center border-2 bg-blue-400'>
+              <label className='m-1'>no. of copies</label>
+              <input
+              type={'number'}
+              placeholder="no. of copies"
+              className='m-1 p-1'
+              value={copies}
+              onChange={(e)=> setCopies(e.target.value)}
+              />
+              <label className='m-1'>tokenId</label>
+              <input
+              type={'number'}
+              placeholder="tokenId"
+              className='m-1 p-1'
+              value={tokenId}
+              onChange={(e)=> setTokenId(e.target.value)}
+              />
+              <button onClick={mint} className='bg-blue-900 text-white rounded-md p-2 m-1'>
+                mint
+              </button>
+            </section>
+          </div>
+        ) :(
+          <div></div>
         )
       }
     </div>
